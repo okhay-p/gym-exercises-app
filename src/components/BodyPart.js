b5e58f0d577ec73ad78c251ced1d8da796fed947
Oakkar Phyo <oakkarphyo7@gmail.com>
2023-08-09 14:30:11 +0800

    changed stylings for bodypart cards

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
 
-const BodyPart = () => {
-  return (
-    <div>BodyPart</div>
-  )
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+	return (
+		<Stack
+			type="button"
+			alignItems="center"
+			justifyContent="center"
+			className="bodyPart-card"
+			sx={{
+				borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+				backgroundColor: '#fff',
+				borderBottomLeftRadius: '20px',
+				width: '270px',
+				height: '280px',
+				cursor: 'pointer',
+				gap: '47px'
+			}}
+		>
+			<img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px' }} />
+		</Stack >
+	)
 }
 
 export default BodyPart
