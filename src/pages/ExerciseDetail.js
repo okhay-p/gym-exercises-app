5b6d19c17f0c2c02c5d20b100c7516b3360f156b
Oakkar Phyo <oakkarphyo7@gmail.com>
2023-08-10 17:10:46 +0800

    made youtube search api call

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
@@ -11,6 +11,8 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
 
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -21,6 +23,8 @@ const ExerciseDetail = () => {
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
 
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData);
     }
 
     fetchExercisesData();
@@ -29,7 +33,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
       <SimilarExercises />
     </Box>
   )
