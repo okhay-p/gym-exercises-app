e90b7b55d2d56e17b724ba4daa5554c87e0b7b84
Oakkar Phyo <oakkarphyo7@gmail.com>
2023-08-10 17:27:26 +0800

    added channel names and title

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -35,6 +35,14 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
             rel='noreferrer'
           >
             <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant='h5' color='#000'>
+                {item.video.title}
+              </Typography>
+              <Typography variant='h6' color='#000'>
+                {item.video.channelName}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
